df5cfa5d864fea495196bdc745a56b91c2664852
Confy-Code <isingizweconfy@gmail.com>
2026-04-14 23:53:46 +0200

    Added the root app.py script

--- scripts/app.py
+++ scripts/app.py
@@ -1,15 +1,17 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template
 import os
 
-app = Flask(__name__)
+app = Flask(__name__,
+            template_folder='../templates',
+            static_folder='../scripts')
 
 @app.route('/')
 def index():
-    return send_from_directory('.', 'index.html')
+    return render_template('index.html')
 
-@app.route('/<path:path>')
+@app.route('/scripts/<path:path>')
 def static_files(path):
-    return send_from_directory('.', path)
+    return send_from_directory('../scripts', path)
 
 if __name__ == '__main__':
     app.run(debug=True)
